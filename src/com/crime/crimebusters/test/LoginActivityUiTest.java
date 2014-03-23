af1ad21e43d177644191d1f57ea35bc7c76cd246
gitterchris <Chris@192.168.2.102>
2014-03-23 15:09:15 +0400

    UI test completed for the login page.

---
+++ src/com/crime/crimebusters/test/LoginActivityUiTest.java
@@ -0,0 +1,39 @@
+package com.crime.crimebusters.test;
+
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.hasContentDescription;
+
+import com.crime.crimebusters.LoginActivity;
+import com.crime.crimebusters.R;
+import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class LoginActivityUiTest 
+	extends ActivityInstrumentationTestCase2<LoginActivity> {
+
+	public LoginActivityUiTest() {
+		super(LoginActivity.class);
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		getActivity();
+	}
+	
+	public void testLogin() { 
+		onView(withId(R.id.email)).perform(typeText("crime.buster"));
+		onView(withId(R.id.password)).perform(typeText("test123"));
+		
+		onView(withId(R.id.sign_in_button)).perform(click());
+
+		onView(withId(R.id.report_button)).check(matches(withText("report")));
+	}
+
+}
